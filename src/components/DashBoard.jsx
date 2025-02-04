a2799e05b11c94983aa020aa36e1f23edb75082e
UrePu <darkin31313@gmail.com>
2025-02-04 15:29:55 +0900

    대시보드

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -1,9 +1,63 @@
 import React from "react";
+import styled from "styled-components";
+import PokemonCard from "./PokemonCard";
+import MonsterBall from "./MonsterBall";
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 1200px;
+  background-color: #f6f6f6;
+  border-radius: 25px;
+  margin-top: 30px;
+`;
+const StyledTitle = styled.div`
+  font-size: 20px;
+  font-weight: 800;
+  margin-top: 40px;
+  margin-bottom: 30px;
+  color: red;
+`;
+const StyledList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  gap: 70px;
+  margin-bottom: 30px;
+`;
 
-const DashBoard = ({ data }) => {
-  console.log(data[0]);
+const DashBoard = ({ data, listState }) => {
+  const encodeList = () => {
+    let result = [];
+    let count = 0;
 
-  return <div>에ㅐ혀</div>;
+    listState.selectedPokemon.map((pokemon) => {
+      result.push(pokemon);
+      count++;
+    });
+    if (count < 6) {
+      for (let i = 0; i < 6 - count; i++) {
+        result.push(0);
+      }
+    }
+
+    return result;
+  };
+  return (
+    <StyledContainer>
+      <StyledTitle>나만의 포켓몬</StyledTitle>
+      <StyledList>
+        {encodeList().map((pokemon, index) => {
+          if (pokemon === 0) {
+            return <MonsterBall key={index} />;
+          } else {
+            return <PokemonCard key={index} pokemon={pokemon} list="true" />;
+          }
+        })}
+      </StyledList>
+    </StyledContainer>
+  );
 };
 
 export default DashBoard;
