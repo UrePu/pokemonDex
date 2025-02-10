fbe781823a8e1770eb8d4ef77d1c062bf31b9fdb
UrePu <darkin31313@gmail.com>
2025-02-10 09:57:17 +0900

    customToast

--- src/components/CustomToast.jsx
+++ src/components/CustomToast.jsx
@@ -2,23 +2,27 @@ import React from "react";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import styled from "styled-components";
-const styledToast = styled(ToastContainer)`
-  width: 150px
-  font-size: 20px;
+const StyledToast = styled(ToastContainer)`
+  .Toastify__toast {
+    background-color: #f8f9fa;
+    color: black;
+    width: 400px;
+    border-radius: 10px;
+    font-size: 17px;
+    justify-content: center;
   }
 `;
 const CustomToast = () => {
   return (
-    <ToastContainer
+    <StyledToast
       position="top-center" // 알람 위치 지정
-      autoClose={1500} // 자동 off 시간
+      autoClose={2000} // 자동 off 시간
       hideProgressBar={false} // 진행시간바 숨김
       closeOnClick // 클릭으로 알람 닫기
       rtl={false} // 알림 좌우 반전
       pauseOnFocusLoss // 화면을 벗어나면 알람 정지
       draggable // 드래그 가능
-      pauseOnHover // 마우스를 올리면 알람 정지
-      theme="light" // 알람 테마
+      theme="light"
       // limit={1} // 알람 개수 제한
     />
   );
